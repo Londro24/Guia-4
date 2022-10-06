5ba93f9a8f1fbb8f3443b87ac12274500a2834a4
Londro24 <alejandro.ide.figueroa@gmail.com>
2022-10-06 15:20:01 -0300

    mas avance

--- general/src/main.rs
+++ general/src/main.rs
@@ -1,9 +1,10 @@
 use rand::Rng;
+use std::io::stdin;
 
 fn aleatorio(mut arreglo: [i32;12]) -> [i32;12] {
     let mut num = rand::thread_rng();
     for index in 0..=11 {
-        let num_ingresar: i32 = num.gen_range(0..=200);
+        let num_ingresar: i32 = num.gen_range(1..=20);
         arreglo[index] = num_ingresar;
     }
     return arreglo;
@@ -36,50 +37,81 @@ fn ordenar(mut arreglo: [i32;12]) -> [i32;12] {
 
         contador += 1;
     }
+
+    for index in 0..=11 {
+        if index != 0 && index != 11 {
+            arreglo_aux[index] = -arreglo_aux[index];
+        }
+    }
     
     return arreglo_aux;
 }
 
-/*
-fn censura(mut arreglo: [&str;12]) -> [&str;12] {
-    let mut arreglo_aux : [&str;12] = ["-1";12];
-    let mut contador: i32 = 0;
-
-
-    loop {
-        if contador != 0 && contador != 11 {
-
+fn numero_negativo(arreglo: [i32;12], mut numero_negativo: i32) -> i32 {
+    for index in 0..=11 {
+        if arreglo[index] < 0 {
+            numero_negativo += 1;
         }
-
-        if contador == 11 {
-            break;
-        }
-
-        contador += 1;
     }
-
-    return ["-1";12];
+    return numero_negativo;
 }
- */
 
+fn mostrar(arreglo: [i32;12]) {
+    for index in 0..=11 {
+        if index == 0 {
+            println!("")
+        }
+        if arreglo[index] > 0 {
+            print!("{} ", arreglo[index]);
+        } else if arreglo[index] > 9 {
+            print!("** ")
+        } else {
+            print!("* ")
+        }
+    }
+}
 
 fn main() {
     let mut arreglo_principal : [i32;12] = [0;12];
+    let mut vidas_perdidas:i32 = 0;
+    let mut numeros_n_1: i32 = 0;
+    let mut numeros_n_2: i32 = 0;
 
     arreglo_principal = aleatorio(arreglo_principal);
     println!("{:?}", arreglo_principal);
 
     arreglo_principal = ordenar(arreglo_principal);
     println!("{:?}", arreglo_principal);
+    println!("");
 
-    let x: usize = 1;
+    mostrar(arreglo_principal);
 
-    for index in 0..=11 {
-        if index != 0 && index != 11 && index != x{
-            print!("** ")
-        } else {
-            print!("{} ", arreglo_principal[index])
+    loop {
+        mostrar(arreglo_principal);
+        let mut numero_texto: String = String::new();
+        stdin().read_line(&mut numero_texto).unwrap();
+        let numero: i32 = numero_texto.trim().parse().unwrap();
+
+        numeros_n_1 = numero_negativo(arreglo_principal, numeros_n_1);
+        
+        if numero < 0 || numeros_n_1 == 0{
+            panic!("El juego termino, perdio {} vidas", vidas_perdidas);
         }
+        //hacer funcion
+
+        for index in 1..=10 {
+            if numero == -arreglo_principal[index] {
+                arreglo_principal[index] = -arreglo_principal[index]
+            }
+        }
+
+        numeros_n_2 = numero_negativo(arreglo_principal, numeros_n_2);
+
+        if numeros_n_1 == numeros_n_2 {
+            vidas_perdidas += 1;
+        }
+
+        mostrar(arreglo_principal);
     }
 
 }
