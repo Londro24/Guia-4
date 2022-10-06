77c308f5dd48be3f7ddea1de999fed672ab71180
Londro24 <alejandro.ide.figueroa@gmail.com>
2022-10-05 23:56:31 -0300

    avance ejercicio 3

--- general/src/main.rs
+++ general/src/main.rs
@@ -1,75 +1,85 @@
 use rand::Rng;
 
-fn aleatorio_para_5(mut arreglo: [i32;5]) -> [i32;5] {
+fn aleatorio(mut arreglo: [i32;12]) -> [i32;12] {
     let mut num = rand::thread_rng();
-    for index in 0..=4 {
+    for index in 0..=11 {
         let num_ingresar: i32 = num.gen_range(0..=200);
         arreglo[index] = num_ingresar;
     }
     return arreglo;
 }
 
-fn aleatorio_para_6(mut arreglo: [i32;6]) -> [i32;6] {
-    let mut num = rand::thread_rng();
-    for index in 0..=5 {
-        let num_ingresar: i32 = num.gen_range(0..=200);
-        arreglo[index] = num_ingresar;
-    }
-    return arreglo;
-}
 
-fn ordenar(mut arreglo_1: [i32;5], mut arreglo_2: [i32;6]) -> [i32;11] {
+fn ordenar(mut arreglo: [i32;12]) -> [i32;12] {
     // momento ordenar
-    let mut arreglo_general : [i32;11] = [0;11];
+    let mut arreglo_aux : [i32;12] = [0;12];
     let mut contador: i32 = 0;
 
     loop {
         let mut num: i32 = 0;
         let mut aux: usize = 0;
-        for index in 0..=10 {
-            if index < 5 && arreglo_1[index] != -1 {
-                if num < arreglo_1[index] {
-                    num = arreglo_1[index];
-                    aux = index;
-                }
-            } else if index > 4 && arreglo_2[index - 5] != -1 {
-                if num < arreglo_2[index - 5] {
-                    num = arreglo_2[index - 5];
+        for index in 0..=11 {
+            if arreglo[index] != -1 {
+                if num < arreglo[index] {
+                    num = arreglo[index];
                     aux = index;
                 }
             }
         }
 
-        if aux < 5 {
-            arreglo_general[contador as usize] = num;
-            arreglo_1[aux] = -1;
-        } else {
-            arreglo_general[contador as usize] = num;
-            arreglo_2[aux - 5] = -1;
-        }
+        arreglo_aux[contador as usize] = num;
+        arreglo[aux] = -1;
 
-        if contador == 10 {
+        if contador == 11 {
             break;
         }
 
         contador += 1;
     }
     
-
-    return arreglo_general;
+    return arreglo_aux;
 }
 
+/*
+fn censura(mut arreglo: [&str;12]) -> [&str;12] {
+    let mut arreglo_aux : [&str;12] = ["-1";12];
+    let mut contador: i32 = 0;
+
+
+    loop {
+        if contador != 0 && contador != 11 {
+
+        }
+
+        if contador == 11 {
+            break;
+        }
+
+        contador += 1;
+    }
+
+    return ["-1";12];
+}
+ */
+
+
 fn main() {
-    let mut arreglo_1 : [i32;5] = [0;5];
-    let mut arreglo_2 : [i32;6] = [0;6];
+    let mut arreglo_principal : [i32;12] = [0;12];
 
-    arreglo_1 = aleatorio_para_5(arreglo_1);
-    arreglo_2 = aleatorio_para_6(arreglo_2);
+    arreglo_principal = aleatorio(arreglo_principal);
+    println!("{:?}", arreglo_principal);
 
-    println!("{:?}", arreglo_1);
-    println!("{:?}", arreglo_2);
+    arreglo_principal = ordenar(arreglo_principal);
+    println!("{:?}", arreglo_principal);
 
-    let arreglo_general: [i32;11] = ordenar(arreglo_1, arreglo_2);
+    let x: usize = 1;
 
-    println!("{:?}", arreglo_general);
+    for index in 0..=11 {
+        if index != 0 && index != 11 && index != x{
+            print!("** ")
+        } else {
+            print!("{} ", arreglo_principal[index])
+        }
+    }
+
 }
